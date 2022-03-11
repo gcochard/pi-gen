559932718704f0068ef7dcc243e0de6963a37015
Greg Cochard <gcochard@users.noreply.github.com>
2022-03-11 17:15:00 -0500

    Follow conventions

--- stage3/00-install-packages/00-run.sh
+++ stage3/00-install-packages/00-run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-cp files/unifi-repo.gpg ${ROOTFS_DIR}/etc/apt/trusted.gpg.d/unifi-repo.gpg
+install -m 644 files/unifi-repo.gpg "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+install -m 644 files/raspbian_stretch_for_mongodb.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/100-ubnt-unifi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 on_chroot << EOF
-echo 'deb http://archive.raspbian.org/raspbian stretch main contrib non-free rpi' >> /etc/apt/sources.list.d/raspbian_stretch_for_mongodb.list
-echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' >> /etc/apt/sources.list.d/100-ubnt-unifi.list
+apt-get update
 EOF
