37229758c73320bf890d4a9f8f33ecba5a466e85
Greg Cochard <gcochard@users.noreply.github.com>
2022-03-14 13:16:03 -0400

    Revert "Move stage3 to stage2"
    This reverts commit 0b2552dd244085cb56da542915a9c7079b626373.

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -3,9 +3,8 @@
 install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
 
-# disable waiting for network on boot
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
 EOF
 
 if [ -v WPA_COUNTRY ]; then
